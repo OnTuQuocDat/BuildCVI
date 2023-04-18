eb9ebfd64265d326af90a1cd984ce311145d462b
Dat Tu Quoc On <DAQ@sonion.com>
2023-04-18 15:38:38 +0700

    first

---
+++ Gamma.py
@@ -0,0 +1,41 @@
+def SU60_left():
+    serial_gamma = None
+    print(serial_gamma, "%s", "BBBBBB") 
+    print("Next serial will be: ",serial_gamma)
+    product = 11
+    return product
+
+def SU60_right():
+    serial_gamma = None
+    print(serial_gamma, "%s", "BBBBBB") 
+    print("Next serial will be: ",serial_gamma)
+    product = 11
+    return product
+
+def SU85_L():
+    serial_gamma = None
+    print(serial_gamma, "%s", "BBBBBB") 
+    print("Next serial will be: ",serial_gamma)
+    product = 11
+    return product
+
+def SU85_R():
+    serial_gamma = None
+    print(serial_gamma, "%s", "BBBBBB") 
+    print("Next serial will be: ",serial_gamma)
+    product = 11
+    return product
+
+def SU100_L():
+    serial_gamma = None
+    print(serial_gamma, "%s", "BBBBBB") 
+    print("Next serial will be: ",serial_gamma)
+    product = 11
+    return product
+
+def SU100_R():
+    serial_gamma = None
+    print(serial_gamma, "%s", "BBBBBB") 
+    print("Next serial will be: ",serial_gamma)
+    product = 11
+    return product
